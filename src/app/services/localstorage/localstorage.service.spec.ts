74878ae0cd1bed233aaf7696bf54a3f81176b5e4
Carlos Matheu Armengol <info@plastikaweb.com>
2018-05-26 16:35:19 +0200

    test(services): localstorage service unit testing
    29

--- src/app/services/localstorage/localstorage.service.spec.ts
+++ src/app/services/localstorage/localstorage.service.spec.ts
@@ -1,16 +1,27 @@
 import { inject, TestBed } from '@angular/core/testing';
-import { WindowRefService } from '..';
+import { WindowRefService } from '../windowref/windowref.service';
 import { LocalstorageService } from './localstorage.service';
 
 
 describe('LocalstorageService', () => {
+  let localstorageService: LocalstorageService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ WindowRefService, LocalstorageService ]
     });
+
+    localstorageService = TestBed.get(LocalstorageService);
   });
 
   it('should be created', inject([ LocalstorageService ], (service: LocalstorageService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should set, get and remove an item', () => {
+    localstorageService.setItem('test', 'value');
+    expect(localstorageService.getItem('test')).toBe('value');
+    localstorageService.removeItem('test');
+    expect(localstorageService.getItem('test')).toBeNull();
+  });
 });
