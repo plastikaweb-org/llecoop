5ea0ea3358f1756758fe8e42ae944ee848c5e58e
Carlos Matheu Armengol <info@plastikaweb.com>
2018-08-26 10:59:59 +0200

    test(activity): 44: add effects spec for hide loading indicator if message is present

--- src/app/activity/store/effects/activity/activity.effects.ts
+++ src/app/activity/store/effects/activity/activity.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import * as fromRouter from '@ngrx/router-store';
 import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 import { map, withLatestFrom } from 'rxjs/operators';
 
 import * as fromActions from '../../actions';
@@ -11,13 +11,7 @@ import * as fromSelectors from '../../selectors';
 
 @Injectable()
 export class ActivityEffects {
-  constructor(private actions$: Actions, private store: Store<ActivityState>) {}
-
-  /**
-   * on change route, if any alert message is visible, reset it
-   * @type {Observable<any>}
-   */
-  @Effect({ dispatch: false })
+  @Effect()
   routeGo$ = this.actions$.pipe(
     ofType(fromRouter.ROUTER_NAVIGATION),
     withLatestFrom(
@@ -26,27 +20,19 @@ export class ActivityEffects {
     ),
     map(([ router, visibleError, visibleWarning ]) => {
       if (visibleError) {
-        this.store.dispatch(new fromActions.ResetErrorMessage());
+        return new fromActions.ResetErrorMessage();
       }
       if (visibleWarning) {
-        this.store.dispatch(new fromActions.ResetWarningMessage());
+        return new fromActions.ResetWarningMessage();
       }
+      return of({});
     })
   );
-
-  /**
-   * prevent if alert is shown, the visibility of progress bar
-   * bug when on first load, there is no api connection
-   * @type {Observable<any[]>}
-   */
-  @Effect({ dispatch: false })
+  @Effect()
   showErrorMessage$ = this.actions$.pipe(
     ofType(fromActions.SHOW_ERROR_MESSAGE, fromActions.SHOW_WARNING_MESSAGE),
-    withLatestFrom(this.store.pipe(select(fromSelectors.getLoadingStateVisibility))),
-    map(([ router, visible ]) => {
-      if (visible) {
-        this.store.dispatch(new fromActions.HideLoading());
-      }
-    })
+    map(() => new fromActions.HideLoading())
   );
+
+  constructor(private actions$: Actions, private store: Store<ActivityState>) {}
 }
